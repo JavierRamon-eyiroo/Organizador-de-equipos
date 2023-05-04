0af700c05059bf531582625a3f098db586fd9742
javier <javier.ramon@eyiroo.com>
2023-05-04 12:34:13 -0500

    "Creacion de los colores de fondo de los equipos"

--- src/App.js
+++ src/App.js
@@ -13,17 +13,56 @@ function App() {
   const cambiarMostrar = () => {
     actualizarMostrar(!mostrarFormulario)
   }
+
+  //Lista de equipos
+  const equipos = [
+    {
+      titulo:"Programacion.",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
+    },
+    {
+      titulo:"Front End.",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    },
+    {
+      titulo:"Data Science.",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    },
+    {
+      titulo:"Devops.",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+    {
+      titulo:"Ux y Diseno.",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },
+    {
+      titulo:"Movil.",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },
+    {
+      titulo:"Innovacion y Gestion.",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    }
+  ]
+
   return (
     <div >
       <Header />
       {/*mostrarFormulario ? <Formulario /> : <></>*/}
       {mostrarFormulario && <Formulario />}
       <MiOrg cambiarMostrar={cambiarMostrar} />
-      <Equipo equipo="Programacion"/>
-      <Equipo equipo="Front End"/>
-      <Equipo equipo="Data Science"/>
-      <Equipo equipo="Devops"/>
-      <Equipo equipo="UX y Diseno"/>
+      
+      { 
+        equipos.map((equipo) => <Equipo datos={equipo} key={equipo.titulo}/>)
+      }
     </div>
   );
 }
