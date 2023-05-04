3c7bebb66d41d9187956314dc68e5315f46225db
javier <javier.ramon@eyiroo.com>
2023-05-04 11:02:25 -0500

    "Creacion de componente equipo"

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
 import MiOrg from './componentes/MiOrg';
+import Equipo from './componentes/Equipo';
 
 function App() {
-  const [mostrarFormulario,actualizarMostrar] = useState(true)
+  const [mostrarFormulario,actualizarMostrar] = useState(false)
 
   //Ternario --> condicion ? seMuestra : noSeMuestra
   //condicion && seMuestra
@@ -18,6 +19,11 @@ function App() {
       {/*mostrarFormulario ? <Formulario /> : <></>*/}
       {mostrarFormulario && <Formulario />}
       <MiOrg cambiarMostrar={cambiarMostrar} />
+      <Equipo equipo="Programacion"/>
+      <Equipo equipo="Front End"/>
+      <Equipo equipo="Data Science"/>
+      <Equipo equipo="Devops"/>
+      <Equipo equipo="UX y Diseno"/>
     </div>
   );
 }
